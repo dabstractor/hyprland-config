7c13adac087b08210aa405b1fae2a799b5a409e8
Dustin Schultz <dustindschultz@gmail.com>
2024-06-08 09:43:03 -0400

    split alacritty out into its own script

---
+++ scripts/alacritty.sh
@@ -0,0 +1,7 @@
+hyprctl keyword windowrule "workspace special silent, alacritty"
+hyprctl dispatch exec alacritty
+
+sleep 3
+hyprctl keyword windowrule "workspace unset, alacritty"
+
+hyprctl dispatch movetoworkspace special alacritty
--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,9 +1,4 @@
-
-hyprctl keyword windowrule "workspace special silent, alacritty"
-hyprctl dispatch exec alacritty
-
-sleep 3
-hyprctl keyword windowrule "workspace unset, alacritty"
+source ./alacritty.sh
 
 # Set the wallpaper image
 swaybg -i ~/Pictures/wallpapers/pexels-rejilal-ravi-118052-2575955.jpg -m fill &
