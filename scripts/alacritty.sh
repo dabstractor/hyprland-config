259b63608b72606f4ff091231583d76a0f227adb
Dustin Schultz <dustindschultz@gmail.com>
2024-06-12 14:33:00 -0400

    updated alacritty special workspace

--- scripts/alacritty.sh
+++ scripts/alacritty.sh
@@ -1,7 +1,10 @@
 hyprctl keyword windowrule "workspace special silent, alacritty"
 hyprctl dispatch exec alacritty
-
-sleep 3
+sleep 1
 hyprctl keyword windowrule "workspace unset, alacritty"
 
 hyprctl dispatch movetoworkspace special alacritty
+
+hyprctl dispatch togglespecialworkspace # hide it by default
+
+hyprctl dispatch fullscreen
