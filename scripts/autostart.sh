bd49cd52cc60bf3c5cdfe8a0c06ba70f28505e2a
Dustin Schultz <dustindschultz@gmail.com>
2024-06-23 13:19:13 -0400

    switched from special workspace to hyprscratch

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,5 +1,3 @@
-source ~/.config/hypr/scripts/alacritty.sh
-source ~/.config/hypr/scripts/chat.sh
-
 # Set the wallpaper image
 swaybg -i ~/Pictures/wallpapers/pexels-rejilal-ravi-118052-2575955.jpg -m fill &
+
--- scripts/chat.sh
+++
@@ -1,9 +0,0 @@
-hyprctl keyword windowrule "workspace special silent, chat"
-hyprctl dispatch exec alacritty
-
-hyprctl keyword windowrule "workspace unset, chat"
-
-hyprctl dispatch movetoworkspace special chat
-hyprctl dispatch fullscreen
-hyprctl dispatch togglespecialworkspace # hide it by default
-
